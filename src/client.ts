6f8fe461d3800ed0a2362b26035084ee0bd700a7
Michael Ahern <michael.ahern@gmail.com>
2025-05-25 20:50:23 -0400

    v0.1.1 (#2)

--- src/client.ts
+++ src/client.ts
@@ -11,7 +11,7 @@ if (!clientId || !clientSecret) {
 
 const transport = new StdioClientTransport({
     command: 'node',
-    args: ['./lib/module.js'],
+    args: ['./bin/server.js'],
     env: {
         AIRTHINGS_CLIENT_ID: clientId,
         AIRTHINGS_CLIENT_SECRET: clientSecret,
